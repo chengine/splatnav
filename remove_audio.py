358ff2e276192410d84654dc5bb05eab68aad9ae
chengine <chengine@stanford.edu>
2024-12-17 15:57:36 -0800

    removed audio

--- remove_audio.py
+++ remove_audio.py
@@ -1,7 +1,9 @@
 #%%
 from moviepy.editor import VideoFileClip
+import glob
 
-videoclip = VideoFileClip("static/videos/trial2_clip.mov")
-new_clip = videoclip.without_audio()
-new_clip.write_videofile("static/videos/trial2_clip_.mov", codec="libx264")
+for filepath in glob.glob("static/videos/*.mp4"):
+    videoclip = VideoFileClip(filepath)
+    new_clip = videoclip.without_audio()
+    new_clip.write_videofile(filepath.replace(".mp4", "_noaudio.mp4"), codec="libx265")
 # %%
